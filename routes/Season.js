58994bf69f60f3d472cee7ce06be3e6d1a38490f
akshithareddyb <itsmeakshithareddy@gmail.com>
2021-11-29 09:19:22 -0600

    Modified index and season js file

--- routes/Season.js
+++ routes/Season.js
@@ -1,7 +1,13 @@
 var express = require('express');
 var router = express.Router();
 const Season_controlers= require('../controllers/Season'); 
-
+const secured = (req, res, next) => { 
+    if (req.user){ 
+      return next(); 
+    } 
+    req.session.returnTo = req.originalUrl; 
+    res.redirect("/login"); 
+  }   
 /* GET home page. */
 router.get('/', Season_controlers.Season_view_all_Page ); 
 /* GET detail View page  */  
@@ -9,7 +15,7 @@ router.get('/detail', Season_controlers.Season_view_one_Page);
 /* GET create Season page */ 
 router.get('/create', Season_controlers.Season_create_Page); 
 /* GET create update page */ 
-router.get('/update', Season_controlers.Season_update_Page); 
+router.get('/update',secured, Season_controlers.Season_update_Page); 
  /* GET create Season page */ 
 router.get('/delete', Season_controlers.Season_delete_Page); 
 module.exports = router;
